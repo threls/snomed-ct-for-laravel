90b16053d28b790a62a48c12c81c2d41c610516b
Mark Bonnici <mb6792@gmail.com>
2023-06-28 15:47:56 +0200

    feat(snomed): added ab ility to index while persisting records

--- src/Commands/PersistCommand.php
+++ src/Commands/PersistCommand.php
@@ -12,7 +12,7 @@ use Threls\SnomedCTForLaravel\Models\SnomedIndex;
 
 class PersistCommand extends Command
 {
-    protected $signature = 'snomed:persist {--chunk=5000}';
+    protected $signature = 'snomed:persist {--chunk=5000} {--index}';
 
     protected $description = 'Move snomed indices table to MySQL';
 
@@ -25,6 +25,7 @@ class PersistCommand extends Command
     public function handle()
     {
         $chunk = $this->option('chunk');
+        $doIndex = $this->option('index');
 
         $tempConnection = Config::get('snomed-ct-for-laravel.db.temp.connection');
         $persistedConnection = Config::get('snomed-ct-for-laravel.db.persisted.connection');
@@ -41,39 +42,76 @@ class PersistCommand extends Command
 
         $query = $this->tempDB->table($this->table);
 
-        if (! is_null($persistedEffectiveTime)) {
+        if (!is_null($persistedEffectiveTime)) {
             $query->where('effective_time', '>', $persistedEffectiveTime);
         }
 
         $bar = $this->output->createProgressBar($query->count());
 
-        $query->chunkById($chunk, fn ($rows) => $this->persist($rows, $bar));
+        $query->chunkById($chunk, fn($rows) => $this->persist($rows, $bar, $doIndex));
         $bar->finish();
     }
 
-    public function persist(Collection $chunk, ProgressBar &$progressBar): void
+    public function persist(Collection $chunk, ProgressBar &$progressBar, bool $doIndex): void
     {
-
-        $array = $chunk->map(fn ($row) => [
-            'id' => $row->id,
-            'effective_time' => $row->effective_time,
-            'concept_id' => $row->concept_id,
-            'type_id' => $row->type_id,
-            'term' => $row->term,
-            'semantic_tag' => $row->semantic_tag,
-            'refset_id' => $row->refset_id,
-            'acceptability_id' => $row->acceptability_id,
-            'fsn_id' => $row->fsn_id,
-            'fsn_semantic_tag' => $row->fsn_semantic_tag,
-            'active' => $row->active,
-            'concept_active' => $row->concept_active,
+        $array = $chunk->map(fn($row) => [
+            'id'                     => $row->id,
+            'effective_time'         => $row->effective_time,
+            'concept_id'             => $row->concept_id,
+            'type_id'                => $row->type_id,
+            'term'                   => $row->term,
+            'semantic_tag'           => $row->semantic_tag,
+            'refset_id'              => $row->refset_id,
+            'acceptability_id'       => $row->acceptability_id,
+            'fsn_id'                 => $row->fsn_id,
+            'fsn_semantic_tag'       => $row->fsn_semantic_tag,
+            'active'                 => $row->active,
+            'concept_active'         => $row->concept_active,
             'refset_language_active' => $row->refset_language_active,
         ]);
 
+
+        if ($doIndex) {
+            $this->saveAndIndex($array, $progressBar);
+        } else {
+            $this->upsert($array, $progressBar);
+        }
+    }
+
+    protected function saveAndIndex(Collection $chunk, ProgressBar &$progressBar)
+    {
+        $chunk->each(function (array $record) use (&$progressBar) {
+            /** @var SnomedIndex $snomedIndex */
+            $snomedIndex = app(Config::get('snomed-ct-for-laravel.models.index'));
+
+            $snomedIndex->updateOrCreate(
+                ['id' => $record['id']],
+                [
+                    'effective_time'         => $record['effective_time'],
+                    'concept_id'             => $record['concept_id'],
+                    'type_id'                => $record['type_id'],
+                    'term'                   => $record['term'],
+                    'semantic_tag'           => $record['semantic_tag'],
+                    'refset_id'              => $record['refset_id'],
+                    'acceptability_id'       => $record['acceptability_id'],
+                    'fsn_id'                 => $record['fsn_id'],
+                    'fsn_semantic_tag'       => $record['fsn_semantic_tag'],
+                    'active'                 => $record['active'],
+                    'concept_active'         => $record['concept_active'],
+                    'refset_language_active' => $record['refset_language_active'],
+                ]
+            );
+
+            $progressBar->advance();
+        });
+    }
+
+    protected function upsert(Collection $chunk, ProgressBar &$progressBar)
+    {
         /** @var SnomedIndex $snomedIndex */
         $snomedIndex = app(Config::get('snomed-ct-for-laravel.models.index'));
         $snomedIndex->upsert(
-            $array->toArray(),
+            $chunk->toArray(),
             ['id'],
             [
                 'effective_time',
