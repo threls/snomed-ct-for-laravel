920372eae647a44c82ab9d79b9f6ec420a13376f
Mark Bonnici <mb6792@gmail.com>
2023-06-28 12:10:46 +0200

    feat(snomed): snomed index fsn tags fix

--- src/Commands/SnomedIndexCommand.php
+++ src/Commands/SnomedIndexCommand.php
@@ -11,6 +11,8 @@ use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Threls\SnomedCTForLaravel\Enums\DescriptionType;
+use Threls\SnomedCTForLaravel\Jobs\ImportSnomedJob;
+use Threls\SnomedCTForLaravel\Models\SnomedIndex;
 
 class SnomedIndexCommand extends Command
 {
@@ -36,7 +38,7 @@ class SnomedIndexCommand extends Command
         $this->indexTable(tableName: 'snomed_text_definition', since: $lastUpdate);
 
         $this->info('Linking FSN');
-        $this->linkFsn();
+        $this->linkFsn($lastUpdate);
     }
 
     protected function getLastUpdate(): ?Carbon
@@ -52,7 +54,7 @@ class SnomedIndexCommand extends Command
 
     protected function getChunk(): int
     {
-        return (int) $this->option('chunk');
+        return (int)$this->option('chunk');
     }
 
     protected function indexTable(string $tableName, ?Carbon $since): void
@@ -70,13 +72,13 @@ class SnomedIndexCommand extends Command
                 'snomed_snap_concept.active as snomed_snap_concept_active',
             ]);
 
-        if (! is_null($since)) {
+        if (!is_null($since)) {
             $builder->where("{$tableName}.effectiveTime", '>', $since->endOfDay());
         }
 
         $bar = $this->output->createProgressBar($builder->count());
 
-        $builder->chunk($this->getChunk(), fn ($rows) => $this->index($rows, $bar));
+        $builder->chunk($this->getChunk(), fn($rows) => $this->index($rows, $bar));
 
         $bar->finish();
 
@@ -93,21 +95,21 @@ class SnomedIndexCommand extends Command
                 preg_match('/\([a-zA-Z\/\s]*\)$/', $row->term, $match);
                 if (count($match) != 0) {
                     $semanticTag = substr($match[0], 1, -1);
-                    $row->term = preg_replace('/ '.preg_quote($match[0], '/').'$/', '', $row->term);
+                    $row->term = preg_replace('/ ' . preg_quote($match[0], '/') . '$/', '', $row->term);
                 }
             }
 
             $records->push([
-                'id' => $row->id,
-                'effective_time' => Carbon::parse($row->effectiveTime)->startOfDay(),
-                'concept_id' => $row->conceptId,
-                'type_id' => $row->typeId,
-                'term' => $row->term,
-                'refset_id' => $row->refsetId,
-                'semantic_tag' => $semanticTag,
-                'acceptability_id' => $row->acceptabilityId,
-                'active' => $row->active,
-                'concept_active' => $row->snomed_snap_concept_active ?? false,
+                'id'                     => $row->id,
+                'effective_time'         => Carbon::parse($row->effectiveTime)->startOfDay(),
+                'concept_id'             => $row->conceptId,
+                'type_id'                => $row->typeId,
+                'term'                   => $row->term,
+                'refset_id'              => $row->refsetId,
+                'semantic_tag'           => $semanticTag,
+                'acceptability_id'       => $row->acceptabilityId,
+                'active'                 => $row->active,
+                'concept_active'         => $row->snomed_snap_concept_active ?? false,
                 'refset_language_active' => $row->snomed_refset_language_active ?? false,
             ]);
         });
@@ -125,20 +127,58 @@ class SnomedIndexCommand extends Command
         $progressBar->advance($chunk->count());
     }
 
-    public function linkFsn()
+    public function linkFsn(?Carbon $since)
     {
-        $sql = <<<'SQL'
-update snomed_indices
-set fsn_id           = (select s2.id
-                        from snomed_indices s1
-                                 inner join snomed_indices s2 on s1.concept_id = s2.concept_id
-                            and s2.type_id = 900000000000003001),
-    fsn_semantic_tag = (select s2.semantic_tag
-                        from snomed_indices s1
-                                 inner join snomed_indices s2 on s1.concept_id = s2.concept_id
-                            and s2.type_id = 900000000000003001);
-SQL;
 
-        DB::connection($this->connection)->statement($sql);
+        $query = DB::connection($this->connection)->table(DB::raw("snomed_indices as s1 INDEXED BY 'snomed_indices_effective_time_index'"))->join('snomed_indices as s2', function ($q) {
+            $q->on('s1.concept_id', '=', 's2.concept_id')
+                ->where('s2.type_id', '=', DescriptionType::FULLY_SPECIFIED_NAME->value);
+        })->select(
+            's1.id',
+            's1.effective_time',
+            's1.concept_id',
+            's1.type_id',
+            's1.term',
+            's1.semantic_tag',
+            's1.refset_id',
+            's1.acceptability_id',
+            's1.active',
+            's1.concept_active',
+            's1.refset_language_active',
+            's2.id as fsn_id',
+            's2.semantic_tag as fsn_semantic_tag'
+        )->orderBy('s1.effective_time');
+
+
+        if (!is_null($since)) {
+            $query->where("s1.effective_time", '>', $since->endOfDay());
+        }
+
+        $query->chunk($this->getChunk(), function (Collection $chunk) {
+            $chunk = $chunk->map(
+                fn($row) => [
+                    'id'                     => $row->id,
+                    'effective_time'         => $row->effective_time,
+                    'concept_id'             => $row->concept_id,
+                    'type_id'                => $row->type_id,
+                    'term'                   => $row->term,
+                    'semantic_tag'           => $row->semantic_tag,
+                    'refset_id'              => $row->refset_id,
+                    'acceptability_id'       => $row->acceptability_id,
+                    'active'                 => $row->active,
+                    'concept_active'         => $row->concept_active,
+                    'refset_language_active' => $row->refset_language_active,
+                    'fsn_id'                 => $row->fsn_id,
+                    'fsn_semantic_tag'       => $row->fsn_semantic_tag
+                ]
+            );
+
+            DB::connection($this->connection)->table('snomed_indices')->upsert($chunk->toArray(), ['id'], [
+                'fsn_id',
+                'fsn_semantic_tag',
+            ]);
+
+            $this->output->write('|');
+        });
     }
 }
